a6101ee71134f63a14fbb9859d2150b67eb4f9da
Edwin Lin <edwinlin@gmail.com>
2019-02-15 14:28:41 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,8 +9,8 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
 def won?(board)
   
   WIN_COMBINATIONS.each do |combo|
-    if combo.all? do |index| 
-      board[index] == board[combo[0]]
+    if combo.all? do {|index| board[index] == "X"}
+      return combo
     elsif combo.all? { |index| board[index] == "O"}
       return combo
     end
